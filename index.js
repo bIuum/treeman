7ce41d5df7ed67ecdb4b212742dd85f73ada3f7b
bIuum <avivblum1@gmail.com>
2020-03-11 13:19:48 +0200

    initial commit #3

--- index.js
+++ index.js
@@ -1,18 +1,5 @@
 const treeScanMethods = require('./treeScanMethods');
-const path = require('path');
 
-const args = {
-  dirName: '../desktop-proj',
-  ignoreList: ['node_modules']
-}
-
-treeScanMethods.folderTreeScan(args, (err, file) => {
-  if(err) console.log('Error!');
-
-  else {
-    if(path.extname(file) === '.txt') console.log(file);
-  }
-});
 
 module.exports = {
     ...treeScanMethods
