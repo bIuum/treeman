04ac7bc0ac172e1d6ec83732060d99341611ed56
bIuum <avivblum1@gmail.com>
2020-03-12 08:54:18 +0200

    file utils

--- utils/fileUtils.ts
+++ utils/fileUtils.ts
@@ -1,3 +1,14 @@
+// output, true if file name or path ends with ext, false otherwise
+// works with extensions with 2 dots for example: '.test.js' , '.d.ts'
+
 export function hasExt(file: string, ext: string): boolean {
     return file.endsWith(ext);
+}
+
+
+// output: true if two lines have the same characters in order, false otherwise
+// ignores whitespaces
+
+export function compareLines(line1: string, line2: string): boolean {
+    return line1.replace(/\s/g, '') === line2.replace(/\s/g, '');
 }
