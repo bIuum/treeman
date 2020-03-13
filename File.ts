2a30d88a703881e4a4e02da110b086ec45befbac
bIuum <avivblum1@gmail.com>
2020-03-13 22:51:55 +0200

    file methods: replace, delete, editLine

--- File.ts
+++ File.ts
@@ -1,10 +1,11 @@
 import fs from 'fs';
 import path from 'path';
 
-import { splitLines, getExtension } from './utils/utils';
+import { splitLines, joinLines, getExtension } from './utils/utils';
 
 class File {
     public path: string;
+    public encoding: string;
     public name: string;
     public data: string;
     public ext: string;
@@ -15,6 +16,19 @@ class File {
         const data = fs.readFileSync(filePath, encoding);
 
         this.path = filePath;
+        this.encoding = encoding;
+        this.name = path.basename(filePath);
+        this.data = data;
+        this.ext = getExtension(filePath);
+        this.line = splitLines(data);
+        this.lines = this.line.length;
+    }
+
+    private update(filePath: string = this.path, encoding: string = 'utf-8') {
+        const data = fs.readFileSync(filePath, encoding);
+
+        this.path = filePath;
+        this.encoding = encoding;
         this.name = path.basename(filePath);
         this.data = data;
         this.ext = getExtension(filePath);
@@ -27,6 +41,41 @@ class File {
         const trimStr = str.replace(/\s/g, '');
 
         return trimData.includes(trimStr);
+    }
+
+    replace(strFrom: string, strTo: string): void {
+        const replaced = this.data.replace(strFrom, strTo);
+
+        try {
+            fs.writeFileSync(this.path, replaced);
+            this.update();
+        } catch(err) {
+            console.error(`ERROR: Failed to write into ${this.path} while replacing data.`);
+            return;
+        }
+    }
+
+    delete(): void {
+        try {
+            fs.unlinkSync(this.path);
+        } catch {
+            console.error(`ERROR: Failed to delete ${this.path}.`)
+            return;
+        }
+    }
+
+    editLine(index: number, line: string): void {
+        const editedLines = [...this.line];
+        editedLines[index] = line;
+
+        const editedData = joinLines(editedLines);
+        try {
+            fs.writeFileSync(this.path, editedData);
+            this.update();
+        } catch {
+            console.error(`ERROR: Failed to write into ${this.path} white editing line #${index}`);
+            return;
+        }
     }
 }
 
