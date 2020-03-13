2e5217a96fa7c560c0c44f87a0394ee613b57c0e
bIuum <avivblum1@gmail.com>
2020-03-13 16:21:50 +0200

    File update

--- File.ts
+++ File.ts
@@ -6,18 +6,27 @@ import { splitLines, getExtension } from './utils/utils';
 class File {
     public path: string;
     public name: string;
+    public data: string;
+    public ext: string;
     public line: string[];
     public lines: number;
-    public ext: string;
     
     constructor(filePath: string, encoding: string = 'utf-8') {
         const data = fs.readFileSync(filePath, encoding);
 
         this.path = filePath;
         this.name = path.basename(filePath);
+        this.data = data;
         this.ext = getExtension(filePath);
         this.line = splitLines(data);
         this.lines = this.line.length;
+    }
+
+    includes(str: string): boolean {
+        const trimData = this.data.replace(/\s/g, '');
+        const trimStr = str.replace(/\s/g, '');
+
+        return trimData.includes(trimStr);
     }
 }
 
