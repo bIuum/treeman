6b6df2f2ef0dacdcf6dc1f7adbeeca89458669fc
bIuum <avivblum1@gmail.com>
2020-03-14 08:56:17 +0200

    File documentation

--- src/File.ts
+++ src/File.ts
@@ -3,6 +3,7 @@ import path from 'path';
 
 import { splitLines, joinLines, getExtension } from './utils';
 
+/** Class representing a file, used to create a File object during a scan */
 class File {
     public path: string;
     public encoding: string;
@@ -12,6 +13,12 @@ class File {
     public line: string[];
     public lines: number;
     
+    /**
+     * Create an instance of a file.
+     * 
+     * @param {string} filePath - An absolute or relative path to the file.
+     * @param {string} encoding - File's character encoding, defaults to utf-8.
+     */
     constructor(filePath: string, encoding: string = 'utf-8') {
         const data = fs.readFileSync(filePath, encoding);
 
@@ -24,7 +31,15 @@ class File {
         this.lines = this.line.length;
     }
 
-    private update(filePath: string = this.path, encoding: string = 'utf-8') {
+    /**
+     * This is a private methods, used within its instance, by other methods.
+     * This method reconstructs the object, "refreshing" all of its properties.
+     * Used after an update to one of the file's properties that might affect other properties indirectly.
+     * 
+     * @param {string} filePath - File's relative or absolute path. Defaults to current path. 
+     * @param {string} encoding - File's character encoding. Defaults to current encoding.
+     */
+    private update(filePath: string = this.path, encoding: string = this.encoding): void {
         const data = fs.readFileSync(filePath, encoding);
 
         this.path = filePath;
@@ -36,6 +51,12 @@ class File {
         this.lines = this.line.length;
     }
 
+    /**
+     * Check if a string is included in the file. Whitespaces are ignored.
+     * 
+     * @param {string} str - The string that is being searched for in the file's content.
+     * @returns {booleam} True if the string appears at least once inside the file, false if not. 
+     */
     includes(str: string): boolean {
         const trimData = this.data.replace(/\s/g, '');
         const trimStr = str.replace(/\s/g, '');
@@ -43,6 +64,14 @@ class File {
         return trimData.includes(trimStr);
     }
 
+    /**
+     * Replaces the first instance of a string with another string inside the file's content.
+     * NOTE: Just like string.prototype.replace(), it replaces only the first instance. To replace ALL instances, use regex.
+     * Example: Replace all instances of 'Hello' with 'World': File.replace(/Hello/g, 'World');
+     * 
+     * @param {string} strFrom - The string you wish to replace. 
+     * @param {string} strTo - The string that will replace the previous one.
+     */
     replace(strFrom: string, strTo: string): void {
         const replaced = this.data.replace(strFrom, strTo);
 
@@ -55,6 +84,10 @@ class File {
         }
     }
 
+    /**
+     * Deletes the file from memory. 
+     * NOTE: After deletion, the file will not be sent to trash, it will be permanently deleted from memory.
+     */
     delete(): void {
         try {
             fs.unlinkSync(this.path);
@@ -64,6 +97,12 @@ class File {
         }
     }
 
+    /**
+     * Replace a specific line inside the file.
+     * 
+     * @param {number} index - Index of the line you wish to edit.
+     * @param {string} line - The new contents of the line you wish to edit.
+     */
     editLine(index: number, line: string): void {
         const editedLines = [...this.line];
         editedLines.splice(index, 1, line);
@@ -78,6 +117,13 @@ class File {
         }
     }
 
+    /**
+     * Adds a line into the file in a given index.
+     * If the index is in the beginning or middle of a file, all following lines will be pushed one index up.
+     * 
+     * @param {number} index - Index of the line you wish to add. 
+     * @param {string} line - The contents of the line you wish to add.
+     */
     addLine(index: number, line: string): void {
         const editedLines = [...this.line];
         editedLines.splice(index, 0, line);
