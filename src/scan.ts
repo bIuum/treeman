dfbf3ddb166fabde47339c67719de4f32fed5210
bIuum <avivblum1@gmail.com>
2020-03-15 13:52:01 +0200

    scan async function

--- src/scan.ts
+++ src/scan.ts
@@ -1,13 +1,8 @@
-import * as fs from 'fs';
+import { promises as fs } from 'fs';
 import * as path from 'path';
 
 import { includesSubstring } from './utils/utils';
 import File from './File';
-
-/**
-
- */
-
 
 interface ScanInstructions {
   dirPath: string;
@@ -15,7 +10,7 @@ interface ScanInstructions {
 }
 
 /**
-  * This callback function applies to all files found in a tree scan.
+ * This callback function applies to all files found in a tree scan.
  * 
  * @callback fileCallback
  * @param {Object} err - An object representing an error, if no error, this value is null.
@@ -31,30 +26,30 @@ interface ScanInstructions {
  * @param {string[]} [args.ignoreList] - Optional list of files/folder names/extensions to ignore. example: ['node_modules','.css'] will ignore every file inside node_modules, as well as every css file in your tree.
  * @param {fileCallback} cb - A callback that will apply to every file found in the scan.
  */
-export default function scan(args: ScanInstructions, cb: Function) {
+export default async function scan(args: ScanInstructions, cb: Function) {
   const { dirPath, ignoreList = [] } = args;
 
   // read the directory for its children, which are folders and/or files
-  fs.readdir(dirPath, (err, children) => {
-    if (err) return cb(err);
+  try {
+    const children = await fs.readdir(dirPath);
 
     if(children.length === 0) return;
   
     // iterate over the children
-    children.forEach((child) => {
+    for (const child of children) {
       const childPath = path.resolve(dirPath, child);
   
       // ignore if this child is in the ignore list
       if (includesSubstring(ignoreList, childPath)) return;
 
       // check for further information about the child
-      fs.stat(childPath, (err, childStat) => {
-        if (err) return cb(err);
+      try {
+        const childStat = await fs.stat(childPath);
   
         // if it's a directory, call the function recursively with the directory
         const isDir = childStat.isDirectory();
   
-        if(isDir) scan({
+        if(isDir) await scan({
           ...args,
           dirPath: childPath
         }, cb);
@@ -65,7 +60,11 @@ export default function scan(args: ScanInstructions, cb: Function) {
 
           cb(null, file);
         }
-      });
-    });
-  });
+      } catch(err) {
+        cb(err);
+      }
+    }
+  } catch(err) {
+    cb(err);
+  }
 };
